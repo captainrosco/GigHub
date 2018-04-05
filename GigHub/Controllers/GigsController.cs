8c0894aa3d50f2287aac3bc2af0eccdf5f1511a9
Daron Short <daron.short@hotmail.com>
2018-04-05 18:50:02 -0500

    Updated Create Post Method to submit to database

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,18 +1,20 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers {
 
     public class GigsController : Controller {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public GigsController() {
             _context = new ApplicationDbContext();
         }
 
-        // GET: Gigs
+        [Authorize]
         public ActionResult Create() {
 
             var viewModel = new GigFormViewModel {
@@ -20,6 +22,28 @@ namespace GigHub.Controllers {
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel) {
+
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig {
+                Artist = artist,
+                Genre = genre,
+                Venue = viewModel.Venue,
+                DateTime = DateTime.Parse(String.Format("{0}, {1}", viewModel.Date, viewModel.Time))
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
