1abc2333db7db5eb673b892f2beee2b57c4f1577
Daron Short <daron.short@hotmail.com>
2018-04-06 17:53:19 -0500

    Added Valid Time Method to check format

--- GigHub/ViewModels/ValidTime.cs
+++ GigHub/ViewModels/ValidTime.cs
@@ -3,18 +3,16 @@ using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 
 namespace GigHub.ViewModels {
-    public class FutureDate : ValidationAttribute {
+    public class ValidTime : ValidationAttribute {
 
         public override bool IsValid(object value) {
             DateTime dateTime;
             var isValid = DateTime.TryParseExact(Convert.ToString(value),
-                 "d MMM yyyy",
+                 "HH:mm",
                  CultureInfo.CurrentCulture,
                  DateTimeStyles.None, out dateTime);
 
-            return (isValid && dateTime > DateTime.Now);
+            return (isValid);
         }
     }
-
-    
 }
