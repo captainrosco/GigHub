f8e76080cde9f4e54f2752ec6e6cdfb38745972f
Daron Short <daron.short@hotmail.com>
2018-04-09 18:15:25 -0500

    Added Attendences For Gigs/Users

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace GigHub.Models {
 
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false) {
@@ -13,6 +14,14 @@ namespace GigHub.Models {
 
         public static ApplicationDbContext Create() {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gigs)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 
